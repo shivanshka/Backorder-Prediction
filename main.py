0be97aafb0d7ef6163785a301ea355f6b1aa60a7
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-08-18 13:10:11 +0530

    config.yaml added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/")
 def home():
     return "Application is under construction"
 
